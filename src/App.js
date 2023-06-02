ebc5f7cc394e9299f5d5d7c5ca3a583f9654bb1c
ppxf <18828871047@163.com>
2023-06-02 14:22:21 +0800

    feat: first version

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [music, setMusic] = useState(null)
+  const [noHover, setHover] = useState(true)
+
+  const playMusic = () => {
+    if (!music) {
+      let audio = new Audio('images/music.mp3');
+      audio.muted = false;
+      audio.play()
+      setMusic(audio)
+    }
+
+    if (noHover) {
+      setHover(false)
+    } else {
+      setHover(true)
+    }
+  }
+
+  useEffect(() => {
+
+  })
+
+  return (
+    <div>
+      <div className={`box ${noHover ? 'no_hover' : ''}`} onClick={playMusic}>
+        <ul className="minbox">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <ol className="maxbox">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
